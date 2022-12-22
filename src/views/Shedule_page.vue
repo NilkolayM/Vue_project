98b856ccab7bfbc38117f43a627652f576baca01
NilkolayM <113176887+NilkolayM@users.noreply.github.com>
2022-12-23 01:45:56 +0300

    v0.3

--- src/views/Shedule_page.vue
+++ src/views/Shedule_page.vue
@@ -10,12 +10,21 @@
             <th>Состояние</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody :key="UpdateIt">
           <tr v-for="cell in Cells" :key="cell.Date_time">
             <td>{{ cell.Date_time }}</td>
             <td>{{ cell.Place }}</td>
             <td>{{ cell.User_name }}</td>
-            <td>{{ cell.Status }}</td>
+            <td>
+              <button
+                id="bx"
+                class="w-40 btn btn-primary"
+                type="button"
+                @click="CellClicked(cell.Cell_ID)"
+              >
+                {{ GetCellStatus(cell.Status, cell.Client_ID) }}
+              </button>
+            </td>
           </tr>
         </tbody>
       </table>
@@ -30,20 +39,63 @@ export default {
   data() {
     return {
       Cells: [],
+      UpdateIt: false,
     };
   },
   components: {},
-  methods: {},
+  watch: {},
+  methods: {
+    GetCellStatus(status, ID) {
+      if (status == 0) return "Записаться";
+      else if (localStorage.getItem("token") === ID) return "Отменить";
+      else return "Недоступно";
+    },
+    RefreshCells() {
+      axios
+        .get("http://192.168.0.112:5282/api/Shedule")
+        .then((res) => {
+          this.Cells = res.data;
+          this.UpdateTt != this.UpdateTt;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    CellClicked(ID) {
+      axios
+        .put(
+          "http://192.168.0.112:5282/api/Shedule?user1=" +
+            localStorage.getItem("token") +
+            "&cell1=" +
+            ID
+        )
+        .then((res) => {
+          switch (res.data) {
+            case "sign_success":
+              this.RefreshCells();
+              break;
+            case "unsign_success":
+              this.RefreshCells();
+              break;
+            case "wrong_user":
+              alert("Не пользователь");
+              break;
+            case "cant_sign":
+              alert("Записан кто то ещё");
+              break;
+            default:
+              alert("Error!");
+              break;
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
   computed: {},
   mounted: function () {
-    axios
-      .get("http://192.168.0.112:5282/api/Shedule")
-      .then((res) => {
-        this.Cells = res.data;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    this.RefreshCells();
   },
 };
 </script>
