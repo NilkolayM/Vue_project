028be3b0cb06ebc18ae42d5ea2ae656fef6581ee
NilkolayM <113176887+NilkolayM@users.noreply.github.com>
2022-12-22 17:53:43 +0300

    v0.2.1

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import "./registerServiceWorker";
 import router1 from "./router/routerN1";
 import store from "./store";
 import "bootstrap/dist/css/bootstrap.css";
-import "D:/vueJS/Vue_project/node_modules/@popperjs/core/lib";
+import "@popperjs/core/lib";
 import "bootstrap";
 
 createApp(App).use(store).use(router1).mount("#app");
